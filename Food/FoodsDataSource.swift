f79f8d4dd0a49c59119aa93e4ca4ae784ce1f4cd
emre <degirmenci.a.emre@gmail.com>
2019-04-22 23:15:04 +0300

    Add MVC

--- Food/FoodsDataSource.swift
+++ Food/FoodsDataSource.swift
@@ -27,17 +27,7 @@ extension FoodsDataSource: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //        if indexPath.section == 0 {
-        //                        let cell = tableView.dequeueReusableCell(withIdentifier: "MainFoodTableViewCell", for: indexPath) as! MainFoodCollectionViewCell
-        //
-        //                        //            cell.mainFoodCollectionView.delegate = self
-        //                        //            cell.mainFoodCollectionView.dataSource = self
-        //                        //            cell.mainFoodCollectionView.reloadData()
-        //
-        //            cell.mainFoodImage.image =
-        //                        return cell
-        //
-        //                    } else {
+        
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellForFood") as! MainFoodTitleTableViewCell
         let food = foods[indexPath.row]
@@ -47,31 +37,6 @@ extension FoodsDataSource: UITableViewDataSource {
     }
     
 }
-//}
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.section == 0 {
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "MainFoodTableViewCell", for: indexPath) as! MainFoodTableViewCell
-//
-//            //            cell.mainFoodCollectionView.delegate = self
-//            //            cell.mainFoodCollectionView.dataSource = self
-//            //            cell.mainFoodCollectionView.reloadData()
-//            cell.mainFoodCollectionView.tag = indexPath.row
-//            return cell
-//
-//        } else {
-//
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "CellForFood", for: indexPath) as! MainFoodTitleTableViewCell
-//
-//            //            if searching {
-//            //                cell.titleLabel?.text = filtered[indexPath.row]
-//            //                cell.priceLabel?.text = priceFood[indexPath.row].description
-//            //            } else {
-//            cell.titleLabel?.text = searchFoods[indexPath.row]
-//            cell.priceLabel?.text = priceFood[indexPath.row].description
-//            return cell
-//        }
-//
-//    }
 
 extension FoodsDataSource: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
