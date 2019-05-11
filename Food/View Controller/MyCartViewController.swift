9b0b0e85b37f8dbeff5e7d73587f0ad0e9f0386e
emre <degirmenci.a.emre@gmail.com>
2019-05-11 22:47:39 +0300

    Add "Sepette ürün bulunmamaktadır" info to MyCartVC

--- Food/View Controller/MyCartViewController.swift
+++ Food/View Controller/MyCartViewController.swift
@@ -12,6 +12,7 @@ class MyCartViewController: UIViewController, UITableViewDataSource, UITableView
     
     var fromDetailFoodNames = ""
     var fromDetailFoodPrices = ""
+    var backgroundView: UIView?
     
     @IBOutlet weak var myCartTableView: UITableView!
     @IBOutlet weak var totalPriceLabel: UILabel!
@@ -27,15 +28,32 @@ class MyCartViewController: UIViewController, UITableViewDataSource, UITableView
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         myCartTableView.reloadData()
+    }
+    
+  
+    
+   
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if fromSharedFood.count == 0 {
+            myCartTableView.setEmptyView(title: "Sepetinizde ürün bulunmamaktadır", message: "Seçtiğiniz yemekler burada listelenir.")
+        }
+        else {
+            myCartTableView.restore()
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return fromSharedFood.count
+        if fromSharedFood.count == 0 {
+            tableView.setEmptyView(title: "Sepetinizde ürün bulunmamaktadır", message: "Seçtiğiniz yemekler burada listelenir.")
+        }
+        else {
+            tableView.restore()
+        }
         
+        return fromSharedFood.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -56,7 +74,47 @@ class MyCartViewController: UIViewController, UITableViewDataSource, UITableView
             fromSharedFood.remove(at: indexPath.row)
             tableView.beginUpdates()
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            
+            if fromSharedFood.count == 0 {
+                myCartTableView.setEmptyView(title: "Sepetinizde ürün bulunmamaktadır", message: "Seçtiğiniz yemekler burada listelenir.")
+            }
+            else {
+                myCartTableView.restore()
+            }
             tableView.endUpdates()
         }
+    }
+}
+
+extension UITableView {
+    func setEmptyView(title: String, message: String) {
+        let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+        let titleLabel = UILabel()
+        let messageLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = UIColor.red
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+        messageLabel.textColor = UIColor.black
+        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        emptyView.addSubview(titleLabel)
+        emptyView.addSubview(messageLabel)
+        titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
+        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
+        titleLabel.text = title
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        // The only tricky part is here:
+        self.backgroundView = emptyView
+        self.separatorStyle = .none
+    }
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
     }
 }
