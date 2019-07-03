e972541efa7a465db0501e3871551a2c974870ba
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2019-07-03 09:49:16 +0300

    before merge line alignment in ChangePasswordVC

--- Food/View Controller/ChangePassword/ChangePasswordViewController.swift
+++ Food/View Controller/ChangePassword/ChangePasswordViewController.swift
@@ -20,7 +20,7 @@ class ChangePasswordViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
         self.view.addGestureRecognizer(tapGesture)
         
@@ -35,52 +35,52 @@ class ChangePasswordViewController: UIViewController {
     
     @IBAction func changePasswordButton(_ sender: Any) {
         
-            self.isLoading(true)
-            let currentPasswordText = currentPassword.text
-            let newPasswordText = newPassword.text
-            let newPasswordAgainText = newPasswordAgain.text
-            changePasswordProvider.request(.changePassword(currentPasswordText!, newPasswordText!, newPasswordAgainText!)) {
-                [weak self] result in
-                guard self != nil else { return }
-                let statusCode = result.value?.statusCode
-                switch result {
-                case .success(let response):
-                    switch statusCode {
-                    case 200:
-                        self!.isLoading(false)
-                        do {
-                            let data = response.data
-                            let json = try JSON(data: data)
-                            let changePasswordResponse = try JSONDecoder().decode(ChangePasswordServiceResponse.self, from: response.data)
-                            
-                            if (changePasswordResponse.Success!) {
-                                print(json.debugDescription)
-                                self?.showAlert(withTitle: "Başarılı", withMessage: changePasswordResponse.Message!, withAction: "pop")
-                                self!.currentPassword.text = ""
-                                self!.newPassword.text = ""
-                                self!.newPasswordAgain.text = ""
-                            } else {
-                                self?.showAlert(withTitle: "Hata", withMessage: changePasswordResponse.Message!, withAction: "pop")
-                            }
-                        } catch {
-                            print("change password error")
-                        } default :
-                            self!.isLoading(false)
-                        break //Error handler
-                    }
-                    case .failure(let error):
-                        self!.isLoading(false)
-                        self!.showError(error.response!)
-                        self?.showAlert(withTitle: "Hata", withMessage: "Şifre Değiştirilememektedir!", withAction: "pop")
-                        if let code = error.response?.statusCode {
-                            if code == 401 {
-                                self?.showAlert(withTitle: "Hata", withMessage: "Şifre Değiştirilememektedir!", withAction: "pop")
-                            }
+        self.isLoading(true)
+        let currentPasswordText = currentPassword.text
+        let newPasswordText = newPassword.text
+        let newPasswordAgainText = newPasswordAgain.text
+        changePasswordProvider.request(.changePassword(currentPasswordText!, newPasswordText!, newPasswordAgainText!)) {
+            [weak self] result in
+            guard self != nil else { return }
+            let statusCode = result.value?.statusCode
+            switch result {
+            case .success(let response):
+                switch statusCode {
+                case 200:
+                    self!.isLoading(false)
+                    do {
+                        let data = response.data
+                        let json = try JSON(data: data)
+                        let changePasswordResponse = try JSONDecoder().decode(ChangePasswordServiceResponse.self, from: response.data)
+                        
+                        if (changePasswordResponse.Success!) {
+                            print(json.debugDescription)
+                            self?.showAlert(withTitle: "Başarılı", withMessage: changePasswordResponse.Message!, withAction: "pop")
+                            self!.currentPassword.text = ""
+                            self!.newPassword.text = ""
+                            self!.newPasswordAgain.text = ""
+                        } else {
+                            self?.showAlert(withTitle: "Hata", withMessage: changePasswordResponse.Message!, withAction: "pop")
                         }
+                    } catch {
+                        print("change password error")
+                    } default :
+                        self!.isLoading(false)
+                    break //Error handler
+                }
+            case .failure(let error):
+                self!.isLoading(false)
+                self!.showError(error.response!)
+                self?.showAlert(withTitle: "Hata", withMessage: "Şifre Değiştirilememektedir!", withAction: "pop")
+                if let code = error.response?.statusCode {
+                    if code == 401 {
+                        self?.showAlert(withTitle: "Hata", withMessage: "Şifre Değiştirilememektedir!", withAction: "pop")
+                    }
                 }
             }
+        }
     }
-  }
+}
 
 
 
